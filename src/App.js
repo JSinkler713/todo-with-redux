07156d0fd802ba3ce83c2f84f3139b0a1cb21af1
James Sinkler <jamessinkler713@gmail.com>
2020-02-29 12:13:08 -0800

    refactor Todo with redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+import appActions from './redux/actions/appActions';
+import ToDo from './components/ToDo';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = (props) => {
+  return <ToDo {...props} />; 
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    list: state.appReducer.list
+  };
+}
+
+const mapDispatchToProps = {
+  ...appActions
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
